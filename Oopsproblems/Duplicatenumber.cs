959577bd319cb742df295b5260bb951178327e81
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-14 10:39:53 +0530

    [Sachin] Adding Duplicatenumber

---
+++ Oopsproblems/Duplicatenumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopsproblems
+{
+    internal class Duplicatenumber
+    {
+        public void DuplicateFunction()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 1, 2, };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        Console.WriteLine("Find duplicate:" + array[j]);
+                    }
+                }
+            }
+        }
+    }
+}
--- Oopsproblems/Program.cs
+++ Oopsproblems/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the oops problems");
+            Console.WriteLine("Select the program given below");
+            Console.WriteLine("\n1:Pattern\n2:FindmaxminArray\n3:SumofDigits\n4:Reversestringeachword\n5:FindDuplicates");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Duplicatenumber myObj = new Duplicatenumber();
+                    myObj.DuplicateFunction();
+                    break;
+            }
         }
     }
 }
