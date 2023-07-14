c84ebed50382dee870d5fa03bbc8e794243ac00a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-14 10:51:03 +0530

    [Sachin] Adding FirstLetterCapital

---
+++ Oopsproblems/FirstletterCapital.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopsproblems
+{
+    internal class FirstletterCapital
+    {
+        public void FirstLetterCapitalFunction()
+        {
+            string input = "welcome to the rFP batch";
+            string[] words = input.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                if (word.Length > 0)
+                {
+                    string firstLetter = word.Substring(0, 1).ToUpper();
+                    string restOfWord = word.Substring(1).ToLower();
+                    words[i] = firstLetter + restOfWord;
+                }
+            }
+            string output = string.Join(" ", words);
+            Console.WriteLine(output);
+        }
+    }
+}
--- Oopsproblems/Program.cs
+++ Oopsproblems/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to the oops problems");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:Duplicatenumber\n2:FindMaxmin");
+            Console.WriteLine("\n1:Duplicatenumber\n2:FindMaxmin\n3:FirstletterCapital");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -17,6 +17,10 @@
                 case 2:
                     FindMaxmin myObj1 = new FindMaxmin();
                     myObj1.FindMaxMinFunction();
+                    break;
+                case 3:
+                    FirstletterCapital myObj2 = new FirstletterCapital();
+                    myObj2.FirstLetterCapitalFunction();
                     break;
             }
         }
